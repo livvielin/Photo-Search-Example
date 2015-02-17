bd7b79b661a791367592cdc6713f6bf0b04ccb75
Livvie <livvie.lin@gmail.com>
2015-02-16 16:45:02 -0800

    Initial commit

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET( "https://api.instagram.com/v1/tags/cats/media/recent?client_id=bf88a9bc7f58497ba3bb81e2dc40d846",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
+                        }
+                    }
+                    
+                    // display urlArray in ScrollView
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    for var i = 0; i < urlArray.count; i++ {
+                        let imageData = NSData(contentsOfURL: NSURL(string: urlArray[i])!)
+                        if let imageDataUnwrapped = imageData {
+                            let imageView = UIImageView(image: UIImage(data: imageDataUnwrapped))
+                            imageView.frame = CGRectMake(0, 320 * CGFloat(i), 320, 320)
+                            self.scrollView.addSubview(imageView)
+                        }
+                    }
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!, error: NSError!) in
+                println("Error: " + error.localizedDescription)
+            })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +54,3 @@ class ViewController: UIViewController {
 
 
 }
-
---
+++ bridge.h
@@ -0,0 +1,15 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Livvie Lin on 2/16/15.
+//  Copyright (c) 2015 Livvie Lin. All rights reserved.
+//
+
+#import "AFNetworking.h"
+
+#ifndef Photo_Search_Example_bridge_h
+#define Photo_Search_Example_bridge_h
+
+
+#endif
